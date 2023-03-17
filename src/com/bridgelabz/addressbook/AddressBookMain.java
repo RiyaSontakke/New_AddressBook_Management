0bf3d2ed15396e7bf384fc37a7a15574cddce72c
Riya Sontakke <riyaharhare04@gmail.com>
2023-03-17 08:48:15 +0530

    Added:New Contact in AddressBook

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -3,6 +3,8 @@ package com.bridgelabz.addressbook;
 public class AddressBookMain {
     public static void main(String[] args){
         System.out.println("Welcome to Address Book Program..");
+        ContactOperationsImpl contacts=new ContactOperationsImpl ();
+        contacts.addContact();
     }
 
 }
